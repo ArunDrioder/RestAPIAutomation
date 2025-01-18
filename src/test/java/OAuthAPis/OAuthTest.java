395a3eb82cb696b02360ca0c55c9081205154d05
ArunDrioder <arunprasadh.s@gmail.com>
2025-01-18 13:49:28 +0530

    Deserialization

--- src/test/java/OAuthAPis/OAuthTest.java
+++ src/test/java/OAuthAPis/OAuthTest.java
@@ -1,7 +1,15 @@
 package OAuthAPis;
 
+import POJO.Api;
+import POJO.GetCourse;
+import POJO.WebAutomation;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -10,6 +18,8 @@ public class OAuthTest
     @Test
     public void authorizationServerTest()
     {
+
+        String[] actualCourseArrayList = {"Selenium Webdriver Java","Cypress","Protractor"};
 
         //Getting Token
       String response =   given().formParam("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
@@ -29,8 +39,42 @@ public class OAuthTest
 
         //Getting course details response.
 
-        String responseForCourse = given().queryParams("access_token", token)
-                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
-        System.out.println(responseForCourse);
+        GetCourse getCourseResponse = given().queryParams("access_token", token)
+                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourse.class);
+        System.out.println(getCourseResponse);
+
+        System.out.println(getCourseResponse.getLinkedIn());
+        System.out.println(getCourseResponse.getInstructor());
+        System.out.println(getCourseResponse.getCourses().getApi().get(1).getCourseTitle());
+
+        // Let's say if the selected title is in random position in the list, then we need to scan the entire list
+        //to match the title & print it using for loop.
+
+        List<Api> getApiCourse = getCourseResponse.getCourses().getApi();
+
+        for (int i=0; i<getApiCourse.size(); i++)
+        {
+           if (getApiCourse.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+           {
+               System.out.println( getApiCourse.get(i).getPrice());
+           }
+        }
+
+        //printing the title of all the course in the web automation.
+
+        ArrayList<String> al = new ArrayList<String>();
+
+
+        List<WebAutomation> getWebAutomationCourse = getCourseResponse.getCourses().getWebAutomation();
+        for (int i = 0; i< getWebAutomationCourse.size();i++)
+        {
+            System.out.println(getWebAutomationCourse.get(i).getCourseTitle());
+            al.add(getWebAutomationCourse.get(i).getCourseTitle());
+
+        }
+
+        List<String> expectedCourseArrayList = Arrays.asList(actualCourseArrayList);
+
+        Assert.assertTrue(al.equals(expectedCourseArrayList));
     }
 }
